1696988f3f899d6eb5101fa7c4d477005c28337f
jiyounglee-hr <2074titi@naver.com>
2025-04-03 16:10:58 +0900

    Update: 기관제출용 인원현황 페이지의 조회월 기본값을 현재 월로 설정

--- app.py
+++ app.py
@@ -901,7 +901,7 @@ try:
                     st.dataframe(
                         group1_stats,
                         use_container_width=False,
-                        width=720,
+                        width=810,
                         column_config={col: st.column_config.NumberColumn(col, width=50) for col in group1_stats.columns}
                     )
                     
