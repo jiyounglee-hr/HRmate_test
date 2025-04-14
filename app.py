a62035ebe61b18ce788dcf866aa8c3b896bdb064
jiyounglee-hr <2074titi@naver.com>
2025-04-14 19:41:48 +0900

    fix: matplotlib 라이브러리 import 추가

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 from datetime import datetime
 import os
 from office365.runtime.auth.authentication_context import AuthenticationContext
