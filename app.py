09987a77a1ec13780857882a1c51fc6a91ddc4eb
jiyounglee-hr <2074titi@naver.com>
2025-05-03 22:20:28 +0900

    fix: 구글 시트 워크시트 접근 방식 수정 및 디버깅 정보 추가

--- app.py
+++ app.py
@@ -2397,7 +2397,7 @@ try:
                             업무내용 = 업무내용.replace("\n", "<br>")
                             업무내용 = re.sub(r'(https?://\S+)', r'<a href="\1" target="_blank">\1</a>', 업무내용)
                         # '보기>' 텍스트에 링크 심기
-                        업무내용 = 업무내용.replace("링크>", '<a href="URL">링크></a>')
+                        업무내용 = 업무내용.replace("링크", '<a href="URL">링크></a>')
                         html_output.append(f'<td style="width: 85%; text-align: left; padding-left: 15px;">{업무내용}</td>')
                         html_output.append("</tr>")
                     
