f2c268dfab6a18acd0e354ad1f2031960f1491d2
jiyounglee-hr <2074titi@naver.com>
2025-05-06 15:10:18 +0900

    채용전형관리 expander 기본 열림 상태로 수정

--- app.py
+++ app.py
@@ -2573,11 +2573,11 @@ try:
         elif menu == "📝 지원서 관리":
             st.markdown("##### 📝 지원서 관리")
             st.markdown("<br>", unsafe_allow_html=True)
-            with st.expander("🚀 채용전형관리"):
-                st.markdown('<a href="https://hr-resume-uzu5bngyefgcv5ykngnhcd.streamlit.app/" target="_blank" class="sidebar-link" style="text-decoration: none; color: #1b1b1e;">▫️채용 전형 시스템</a>', unsafe_allow_html=True)
-                st.markdown('<a href="https://hr-resume-uzu5bngyefgcv5ykngnhcd.streamlit.app/~/+/?page=admin" target="_blank" class="sidebar-link" style="text-decoration: none; color: #1b1b1e;">▫️면접 평가서 관리</a>', unsafe_allow_html=True)
-                st.markdown('<a href="https://docs.google.com/spreadsheets/d/1zwYJ2hwneCeSgd6p4s9ngll8PDmhLhq9qOTRo5SLCz8/edit?gid=0#gid=0" target="_blank" class="sidebar-link" style="text-decoration: none; color: #1b1b1e;">▫️면접 평가서 DB</a>', unsafe_allow_html=True)
-                st.markdown('<a href="https://docs.google.com/spreadsheets/d/1SfVtvaHgXesDFtdFozt9CJD8aQpPBrK76AxNj-OOfFE/edit?gid=0#gid=0" target="_blank" class="sidebar-link" style="text-decoration: none; color: #1b1b1e;">▫️평가기준 및 채용공고 DB</a>', unsafe_allow_html=True)
+            with st.sidebar.expander("🚀 채용전형관리", expanded=True):
+                st.markdown('<a href="https://hr-resume-uzu5bngyefgcv5ykngnhcd.streamlit.app/" target="_blank" class="sidebar-link" style="text-decoration: none; color: #1b1b1e; font-size: 13px;">▫️채용 전형 시스템</a>', unsafe_allow_html=True)
+                st.markdown('<a href="https://hr-resume-uzu5bngyefgcv5ykngnhcd.streamlit.app/~/+/?page=admin" target="_blank" class="sidebar-link" style="text-decoration: none; color: #1b1b1e; font-size: 13px;">▫️면접 평가서 관리</a>', unsafe_allow_html=True)
+                st.markdown('<a href="https://docs.google.com/spreadsheets/d/1zwYJ2hwneCeSgd6p4s9ngll8PDmhLhq9qOTRo5SLCz8/edit?gid=0#gid=0" target="_blank" class="sidebar-link" style="text-decoration: none; color: #1b1b1e; font-size: 13px;">▫️면접 평가서 DB</a>', unsafe_allow_html=True)
+                st.markdown('<a href="https://docs.google.com/spreadsheets/d/1SfVtvaHgXesDFtdFozt9CJD8aQpPBrK76AxNj-OOfFE/edit?gid=0#gid=0" target="_blank" class="sidebar-link" style="text-decoration: none; color: #1b1b1e; font-size: 13px;">▫️평가기준 및 채용공고 DB</a>', unsafe_allow_html=True)
 
             # 개발 예정 안내 메시지
             st.info("🚧 현재 개발 진행 중입니다.")
