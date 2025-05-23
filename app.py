5fb003347a789ffab5e45a32af675571547ca548
jiyounglee-hr <2074titi@naver.com>
2025-05-23 13:26:19 +0900

    로그인

--- app.py
+++ app.py
@@ -571,7 +571,7 @@ def login():
                 <div class="divider"><hr></div>
     """, unsafe_allow_html=True) 
     st.markdown(f'''
-        <div style="width:100%; text-align:center; margin-top:40px;">
+        <div style="width:100%; text-align:center; ">
             <a href="{auth_url}" target="_blank" class="login-button">Microsoft로 로그인</a>
         </div>
     ''', unsafe_allow_html=True)
